12f9ca6089598fededbe96e3b8731c9cfb3218f2
R-light07 <110286868+R-light07@users.noreply.github.com>
2024-07-22 08:28:41 +0200

    Delete codecr3ator-main directory

--- codecr3ator-main/JS/script.js
+++
@@ -1,6 +0,0 @@
-var menuBar = document.getElementById('menuBar');
-var navlinks = document.getElementById('navlinks');
-
-menuBar.addEventListener('click', function() {
-    navlinks.classList.toggle('open');
-});
--- codecr3ator-main/README.md
+++
@@ -1,2 +0,0 @@
-# codecreator
-website
